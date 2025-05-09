576abcc9d0e6a87cf07eda9530a9c25db3b33079
Leandro <leandrowebmaster@gmail.com>
2025-05-09 15:23:45 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -77,15 +77,9 @@
     (...args:any) => <Elements.ImageBox pass={{
       elementsProperties:[{}],
 
-      styles:[
-              {
-                height: "50px",
-                width: "80px",
-                backgroundColor: "#FDFDFD",
-                borderWidth: 2,
-                borderColor: "#EEE"
-              }
-              ],
+      styles:[{
+  flex: 1,
+}],
 
       URIvariablePath:[`https://firebasestorage.googleapis.com/v0/b/devs-tests-95208.appspot.com/o/images%2FGTA%20VI%20Logo.png?alt=media&token=c74d76cb-f7fe-4aa9-b5c5-4da9e20065f5`],
 
