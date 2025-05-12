991384ab70f04a95648af2514f3094fc1b6b7814
Leandro <leandrowebmaster@gmail.com>
2025-05-12 08:22:26 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -305,8 +305,8 @@ alignItems: "center",
       elementsProperties:[{}],
 
       styles:[{
-width: "356",
-height: "90",
+width: "356px",
+height: "90px",
 }],
 
       URIvariablePath:[`https://firebasestorage.googleapis.com/v0/b/devs-tests-95208.appspot.com/o/images%2FGTAbtnPreOrder.png?alt=media&token=5b9f5b72-aa71-48d2-83e3-074441571300`],
