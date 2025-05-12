e1d1279be93a0b1d72e930ab3d1b11adfc86d56f
Leandro <leandrowebmaster@gmail.com>
2025-05-12 09:35:41 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -170,7 +170,7 @@ alignItems: "center",
   textAlign: "center",
   fontSize: "20px",
   textShadowColor: "black",
-  textShadowOffset: {width: 5, height: 5},
+  textShadowOffset: "{width: 5, height: 5}",
 }`
           ],
 
